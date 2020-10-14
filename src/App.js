da2ce8050d585066102fb17e6f7bc5c8fa9b7ea7
josh <joshua@marinacci.org>
2020-10-14 11:06:14 -0700

    onlly show calendar events on the current day sort calendar events by timestamp support repeating calendar events

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './App.css'
-import {CATEGORIES, DATA} from './schema.js'
 import {ContactList} from './contacts.js'
 import {PeopleBar} from './peoplebar.js'
 import {TaskLists} from './tasks.js'
 import {Chat} from './chat.js'
 import {Calendar} from './calendar.js'
 import {Notes} from './notes.js'
+import {DATA} from './data.js'
 
 function App() {
   return <div>
--- src/calendar.js
+++ src/calendar.js
@@ -3,24 +3,66 @@ import {CATEGORIES} from './schema.js'
 import {DataList, Window} from './ui.js'
 import React from 'react'
 import {formatWithOptions} from 'date-fns/fp'
+import {format, getWeek, startOfWeek, endOfWeek, isWithinInterval, isBefore,
+    setHours, getHours, setMinutes, getMinutes, isAfter
+} from 'date-fns'
 import {AND, query2 as QUERY} from './query2.js'
 
 const isCalendarCategory = () => ({ CATEGORY:CATEGORIES.CALENDAR.ID })
 const isEvent = () => ({ TYPE:CATEGORIES.CALENDAR.TYPES.EVENT })
 
+function toTodayTime (d) {
+    let A = Date.now()
+    A = setHours(A, getHours(d))
+    A = setMinutes(A, getMinutes(d))
+    return A
+}
+function day_time_comparator(a,b) {
+    let A = toTodayTime(a)
+    let B = toTodayTime(b)
+    if(isAfter(A,B)) {
+        return 1
+    } else {
+        return -1
+    }
+}
+
+function is_event_repeating_daily(e) {
+    if(e.props.repeats === 'daily') {
+        return true
+    }
+    return false
+}
+
+
 export function Calendar({data}) {
+    let now = Date.now()
+
     let events = QUERY(data,AND(isCalendarCategory(),isEvent()))
 
-    const dateToString = formatWithOptions({ }, 'h:m a')
+    let start_week = startOfWeek(now)
+    let end_week = endOfWeek(now)
+    let current_week = {start:start_week, end: end_week}
+    console.log(start_week, end_week, current_week)
+    //only show events that are today
+    events.forEach(e => {
+        console.log(e.props.start)
+        // console.log("inside",isWithinInterval(e.props.start,{start:start_week, end: end_week}))
+    })
+
+
+    // only have events that are within the current week
+    events = events.filter(e => {
+        return isWithinInterval(e.props.start,current_week) || is_event_repeating_daily(e)
+    })
+
+    events.sort((a,b) => day_time_comparator(a.props.start,b.props.start))
+
 
     return <Window width={500} height={530} x={650} y={350} title={'calendar'} className={'calendar'}>
-        <h1>October 8th, 2020</h1>
-        <h2>Thursday</h2>
+        <h1>{format(now,'E MMM d')}</h1>
         <DataList data={events}
-                  stringify={o => {
-                      let start = new Date(o.props.start)
-                      return dateToString(start) + ' ' + propAsString(o, 'title')
-                  }}
+                  stringify={e => format(e.props.start,"hh:mm aa") + ' ' + propAsString(e,'title')}
         />
     </Window>
 }
