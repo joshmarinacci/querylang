e132e70167b3123bcdd10d2ce94efd856de38bb2
josh <joshua@marinacci.org>
2020-11-06 13:44:54 -0800

    more cleanup

--- src/apps/DataBrowser.js
+++ src/apps/DataBrowser.js
@@ -45,15 +45,15 @@ function findTypeByKey(selectedCat, key) {
     return CATEGORIES[selectedCat.ID].SCHEMAS[key]
 }
 
-function findTypesForCategory(cat) {
-    let tt = CATEGORIES[cat.ID].SCHEMAS
-    if(!CATEGORIES[cat.ID].SCHEMAS) return [
+function fetch_types_by_category(cat) {
+    if(!cat || !CATEGORIES[cat.ID] || !CATEGORIES[cat.ID].SCHEMAS) return [
         {
             key:'NONE',
             title:'none',
             props:{}
         }
     ]
+    let tt = CATEGORIES[cat.ID].SCHEMAS
     let arr = Object.keys(tt).map(key => {
         tt[key].key = key
         return tt[key]
@@ -67,6 +67,7 @@ function findTypesForCategory(cat) {
 }
 
 function fetch_props_for_type(type) {
+    if(!type || !type.props) return []
     let arr = Object.values(type.props)
     arr.unshift({key:"NONE"})
     return arr
@@ -75,7 +76,6 @@ function fetch_props_for_type(type) {
 function findPropByKey(props, key) {
     console.log(props)
     return props.find(p => p.key === key)
-    // return props[0]
 }
 
 function find_conditions_for_prop(prop) {
@@ -97,64 +97,20 @@ function find_conditions_for_prop(prop) {
 
 function QueryEditorDialog() {
     let db = useContext(DBContext)
-    let cats = fetch_categories()
-    let [types, setTypes] = useState([])
-    let [props, setProps] = useState([])
 
     let [selectedCat, setSelectedCat] = useState({})
     let [selectedType, setSelectedType] = useState({})
-    let [selectedProp, setSelectedProp] = useState({})
-    let [selectedCondition, setSelectedCondition] = useState("equal")
-    let [isTypeError, setIsTypeError] = useState(false)
-    let [conditions, setConditions] = useState([
-        "equal",
-        "not equal",
-        "substring",
-    ])
-    let [value, setValue] = useState("")
+    let [propQuery, setPropQuery] = useState({})
     let [debug_text, set_debug_text] = useState("")
 
-    const chooseCat = (cat_key) => {
-        let cat = fetch_categories().filter(c => c.ID === cat_key)[0]
+    const chooseCat = (cat) => {
         setSelectedCat(cat)
-        if(!CATEGORIES[cat.ID]) {
-            setTypes([])
-            return
-        }
-        setTypes(findTypesForCategory(cat))
     }
     const chooseType = (key) => {
         let type = findTypeByKey(selectedCat,key)
         setSelectedType(type)
-        if(!type) {
-            console.warn(`missing schema for ${selectedCat}:${selectedType}`)
-            setIsTypeError(true)
-            setProps([])
-        } else {
-            setIsTypeError(false)
-            setProps(fetch_props_for_type(type))
-        }
     }
 
-    const chooseProp = (key) => {
-        let prop = findPropByKey(props,key)
-        setSelectedProp(prop)
-        console.log('chose prop',prop)
-        setConditions(find_conditions_for_prop(prop))
-        if(prop.hasOwnProperty('default')) {
-            setValue(prop.default)
-        }
-    }
-
-    const chooseCondition = (cond)=>{
-        setSelectedCondition(cond)
-    }
-    console.log("=========")
-    console.log("selected cat",selectedCat)
-    console.log("selected type", selectedType)
-    console.log("selected prop", selectedProp)
-    console.log("selected prop", selectedCondition)
-    console.log("value is",value)
 
     const run_query = () => {
         let query = {
@@ -167,68 +123,113 @@ function QueryEditorDialog() {
                 },
             ]
         }
-        if(selectedCondition === 'equal') {
-            query.and.push(IS_PROP_EQUAL(selectedProp.key,value))
-        }
-        if(selectedCondition === 'substring') {
-            query.and.push(IS_PROP_SUBSTRING(selectedProp.key,value))
-        }
-        if(selectedCondition === 'is') {
-            query.and.push(IS_PROP_EQUAL(selectedProp.key,value))
-        }
-        console.log("selected prop is",selectedProp)
-        console.log("running query",query)
+        query.and.push(propQuery)
         let items = db.QUERY(query)
-        console.log("returend items",items)
         set_debug_text(""
             +JSON.stringify(query,null,'  ')
             +"\n"
             +JSON.stringify(items,null,'  '))
     }
 
-    let condField = ""
-    if(selectedProp && selectedProp.type === 'STRING') {
-        condField = <input type={"text"} value={value} onChange={(e) => setValue(e.target.value)}/>
-    }
-    if(selectedProp && selectedProp.type === 'BOOLEAN') {
-        condField = <input type={"checkbox"} checked={value}
-                           onChange={e => setValue(e.target.checked)}/>
-
-    }
-
-
     return <div className={'dialog'}>
-        <HBox>
-            <label>category</label>
-            <select value={selectedCat.ID} onChange={e => chooseCat(e.target.value)}>
-                {cats.map((cat)=><option key={cat.ID} value={cat.ID}>{cat.ID}</option>)}
-            </select>
-        </HBox>
-        <HBox>
-            <label>type</label>
-            <select value={selectedType.ID} onChange={e => chooseType(e.target.value)}>
-                {types.map(type=><option key={type.key} value={type.key}>{type.title}</option>)}
-            </select>
-            <Icon className={isTypeError}>{isTypeError?"error":"check_circle"}</Icon>
-        </HBox>
-
-        <HBox>
-            <label>property</label>
-            <select value={selectedProp.key}
-                    onChange={e => chooseProp(e.target.value)}>
-                {props.map(prop => <option key={prop.key}>{prop.key}</option>)}
-            </select>
-            <select value={selectedCondition}
-                    onChange={e => chooseCondition(e.target.value)}>
-                {conditions.map(cond => <option key={cond}>{cond}</option>)}
-            </select>
-            {condField}
-        </HBox>
-
+        <CategoryQueryView selectedCat={selectedCat} choose={chooseCat}/>
+        <TypeQueryView selectedCat={selectedCat} selectedType={selectedType} choose={chooseType}/>
+        <PropertyQueryView type={selectedType} onChanged={setPropQuery}/>
+        <HBox><button>+</button></HBox>
         <HBox>
             <button onClick={run_query}>run</button>
         </HBox>
 
-        <textarea value={debug_text} className={"debug"}></textarea>
+        <textarea value={debug_text} className={"debug"} disabled/>
     </div>
+}
+
+function CategoryQueryView({selectedCat, choose}) {
+    const chooseCat = (cat_key) => {
+        let cat = fetch_categories().filter(c => c.ID === cat_key)[0]
+        choose(cat)
+    }
+    let cats = fetch_categories()
+    return <HBox>
+        <label>category</label>
+        <select value={selectedCat.ID} onChange={e => chooseCat(e.target.value)}>
+            {cats.map((cat)=><option key={cat.ID} value={cat.ID}>{cat.ID}</option>)}
+        </select>
+    </HBox>
+}
+
+function TypeQueryView({selectedCat, selectedType, choose}) {
+    let types = fetch_types_by_category(selectedCat)
+    return <HBox>
+        <label>type</label>
+        <select value={selectedType.ID} onChange={e => choose(e.target.value)}>
+            {types.map(type=><option key={type.key} value={type.key}>{type.title}</option>)}
+        </select>
+    </HBox>
+}
+
+function PropertyQueryView ({type, onChanged}) {
+    let [selectedProp, setSelectedProp] = useState({})
+    let [selectedCondition, setSelectedCondition] = useState("equal")
+    let [conditions, setConditions] = useState([
+        "equal",
+        "not equal",
+        "substring",
+    ])
+    let [value, setValue] = useState("")
+    let props = fetch_props_for_type(type)
+
+    const genQuery = () => {
+        if(selectedCondition === 'equal') {
+            return IS_PROP_EQUAL(selectedProp.key,value)
+        }
+        if(selectedCondition === 'substring') {
+            return IS_PROP_SUBSTRING(selectedProp.key,value)
+        }
+        if(selectedCondition === 'is') {
+            return IS_PROP_EQUAL(selectedProp.key,value)
+        }
+        return {}
+    }
+
+    const chooseProp = (key) => {
+        let prop = findPropByKey(props,key)
+        setSelectedProp(prop)
+        setConditions(find_conditions_for_prop(prop))
+        if(prop.hasOwnProperty('default')) setValue(prop.default)
+    }
+
+    const chooseCondition = (cond)=>{
+        setSelectedCondition(cond)
+    }
+
+    const chooseValue = (val) => {
+        setValue(val)
+    }
+
+    useEffect(()=>{
+        onChanged(genQuery())
+    },[value,selectedProp,selectedCondition])
+
+    let condField = ""
+    if(selectedProp && selectedProp.type === 'STRING') {
+        condField = <input type={"text"} value={value} onChange={(e) => chooseValue(e.target.value)}/>
+    }
+    if(selectedProp && selectedProp.type === 'BOOLEAN') {
+        condField = <input type={"checkbox"} checked={value}
+                           onChange={e => chooseValue(e.target.checked)}/>
+    }
+
+    return <HBox>
+        <label>property</label>
+        <select value={selectedProp.key}
+                onChange={e => chooseProp(e.target.value)}>
+            {props.map(prop => <option key={prop.key}>{prop.key}</option>)}
+        </select>
+        <select value={selectedCondition}
+                onChange={e => chooseCondition(e.target.value)}>
+            {conditions.map(cond => <option key={cond}>{cond}</option>)}
+        </select>
+        {condField}
+    </HBox>
 }
