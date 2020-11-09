2d44b7398185460fd8deb69a100f64a38ba643e8
Josh Marinacci <jmarinacci@lyft.com>
2020-11-09 14:15:59 -0800

    added a simple mapping example updated react and deps

--- src/apps/maps.js
+++ src/apps/maps.js
@@ -2,11 +2,25 @@ import React, {useContext, useEffect, useRef, useState} from 'react'
 import {DBContext, useDBChanged} from '../db.js'
 import {CATEGORIES} from '../schema.js'
 import {Window} from '../ui/window.js'
+import {MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import "./maps.css"
 
 export function MapViewer({app}) {
     let db = useContext(DBContext)
     useDBChanged(db, CATEGORIES.TASKS.ID)
+    const position = [51.505, -0.09]
     return <Window app={app}>
-        <div>some map here</div>
+        <MapContainer center={position} zoom={13} scrollWheelZoom={false} className={'jmap'}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+        )
     </Window>
 }
