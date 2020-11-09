4e0fd19b434636219dd9751756dbb83e2e2a07ed
Josh Marinacci <jmarinacci@lyft.com>
2020-11-09 14:27:19 -0800

    start on bg

--- src/apps/settings.js
+++ src/apps/settings.js
@@ -2,14 +2,27 @@ import React, {useContext, useEffect, useRef, useState} from 'react'
 import {DBContext, useDBChanged} from '../db.js'
 import {CATEGORIES} from '../schema.js'
 import {Window} from '../ui/window.js'
-import {VBox} from '../ui/ui.js'
+import {Toolbar, VBox} from '../ui/ui.js'
 
 export function SettingsApp({app}) {
     let db = useContext(DBContext)
     useDBChanged(db, CATEGORIES.TASKS.ID)
+    let [panel,setPanel] = useState("background")
     return <Window app={app}>
         <VBox scroll>
-            <div>settings here for stuff</div>
+            <Toolbar>
+                <button onClick={()=>setPanel("background")}>background</button>
+            </Toolbar>
+            {renderPanel(panel)}
         </VBox>
     </Window>
+}
+
+
+
+function renderPanel(panel) {
+    if(panel === 'background') {
+        return <div>background</div>
+    }
+    return <div>nothing</div>
 }
