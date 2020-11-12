553ce4321c8ca2e87d98783d73eadb2badae0989
josh <joshua@marinacci.org>
2020-11-12 14:03:42 -0800

    make email use new grid layout fix some bugs in grid and source

--- src/apps/tasks.js
+++ src/apps/tasks.js
@@ -8,12 +8,14 @@ import {
     Panel, Spacer, StandardListItem,
     TextareaPropEditor,
     TextPropEditor,
-    Toolbar,
+    Toolbar, TopToolbar,
     VBox,
     Window
 } from '../ui/ui.js'
 import {AND, IS_CATEGORY, IS_PROP_EQUAL, IS_PROP_SUBSTRING, IS_PROP_TRUE, IS_TYPE, query2 as QUERY} from '../query2.js'
 import Icon from '@material-ui/core/Icon'
+import {Grid3Layout} from '../ui/grid3layout.js'
+import {SourceList} from '../ui/sourcelist.js'
 
 const isProject = () => IS_TYPE(CATEGORIES.TASKS.TYPES.PROJECT)
 const isTask = () => IS_TYPE(CATEGORIES.TASKS.TYPES.TASK)
@@ -57,7 +59,7 @@ export function TaskLists({app}) {
 
     let panel = <Panel grow={true} className={"content-panel"}>nothing selected</Panel>
     if (selectedTask) {
-        panel = <Panel grow={true} className={"content-panel"}>
+        panel = <Panel grow={true} className={"content-panel col3 row2"}>
             <TextPropEditor buffer={selectedTask} prop={'title'} db={db}/>
             <CheckboxPropEditor buffer={selectedTask} prop={'completed'} db={db}/>
             <TextareaPropEditor buffer={selectedTask} prop={'notes'} db={db}/>
@@ -73,32 +75,31 @@ export function TaskLists({app}) {
     const trashTask = () => db.setProp(selectedTask,'deleted',true)
     const archiveTask = () => db.setProp(selectedTask, 'archived',true)
 
-    return         <HBox grow>
-            <DataList data={projects} selected={selectedProject} setSelected={setSelectedProject}
-                      className={"sidebar"}
-                      stringify={((o,i) => <StandardListItem key={i}
-                                                             icon={propAsString(o,'icon')}
-                                                             title={propAsString(o,'title')}/>)}
-            />
-            <VBox className={'sidebar'}>
-                <Toolbar>
-                    <input type={'search'} value={searchTerms} onChange={e => setSearchTerms(e.target.value)}/>
-                    <Icon disabled={selectedProject===null} onClick={addNewTask} className={'no-border'}>add_circle</Icon>
-                </Toolbar>
-                <DataList data={tasks}  selected={selectedTask} setSelected={setSelectedTask}
-                          stringify={(o,i) => <StandardListItem key={i}
-                                                               icon={(propAsBoolean(o,'completed')?"check_box":"check_box_outline_blank")}
-                                                               title={propAsString(o,'title')}/>}
-                />
+    return <Grid3Layout>
+        <SourceList column={1} data={projects} selected={selectedProject} setSelected={setSelectedProject}
+                    renderItem={(o)=>{
+                        return <StandardListItem
+                            icon={propAsString(o,'icon')}
+                            title={propAsString(o,'title')}/>
+                    }}/>
 
-            </VBox>
-            <VBox style={{flex:1}}>
-                <Toolbar>
-                    <Spacer/>
-                    <Icon onClick={archiveTask}>archive</Icon>
-                    <Icon onClick={trashTask}>delete</Icon>
-                </Toolbar>
-                {panel}
-            </VBox>
-        </HBox>
+        <TopToolbar column={2}>
+            <input type={'search'} value={searchTerms} onChange={e => setSearchTerms(e.target.value)}/>
+            <Icon disabled={selectedProject===null} onClick={addNewTask} className={'no-border'}>add_circle</Icon>
+        </TopToolbar>
+        <TopToolbar column={3}>
+            <Spacer/>
+            <Icon onClick={archiveTask}>archive</Icon>
+            <Icon onClick={trashTask}>delete</Icon>
+        </TopToolbar>
+
+        <SourceList column={2} data={tasks} selected={selectedTask} setSelected={setSelectedTask}
+                    renderItem={(o)=>{
+                        return <StandardListItem
+                            icon={(propAsBoolean(o,'completed')?"check_box":"check_box_outline_blank")}
+                            title={propAsString(o,'title')}/>
+
+                    }}/>
+            {panel}
+    </Grid3Layout>
 }
