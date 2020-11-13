f6b5db7dae8c235f44cee62e9eb621faea4ffea7
Josh Marinacci <jmarinacci@lyft.com>
2020-11-13 11:38:12 -0800

    add contact data example

--- src/stories/StandardViewPanel.stories.js
+++ src/stories/StandardViewPanel.stories.js
@@ -14,7 +14,7 @@ export default {
 
 let db = makeDB()
 
-const gen_data =() => {
+const make_task =() => {
     let task = db.make(CATEGORIES.TASKS.ID, CATEGORIES.TASKS.TYPES.TASK)
     setProp(task,'title',"a cool task")
     setProp(task,'completed',true)
@@ -22,20 +22,73 @@ const gen_data =() => {
     return task
 }
 
+function make_contact() {
+    let person = db.make(CATEGORIES.CONTACT.ID, CATEGORIES.CONTACT.TYPES.PERSON)
+    setProp(person,'first','Josh')
+    setProp(person,'last','Marinacci')
+    setProp(person, 'favorite',true)
+    setProp(person, 'timezone', 'Europe/Berlin')
+    setProp(person,'emails',[
+        {
+            type:CATEGORIES.CONTACT.TYPES.EMAIL,
+            props:{
+                type:'personal',
+                value:"joshua@marinacci.org",
+            },
+        },
+        {
+            type:CATEGORIES.CONTACT.TYPES.EMAIL,
+            props:{
+                type:'work',
+                value:"josh@josh.earth",
+            },
+        }
+    ])
+    setProp(person,'phones',[
+        {
+            type:CATEGORIES.CONTACT.TYPES.PHONE,
+            props: {
+                type: 'personal',
+                value: '707-509-9627'
+            }
+        }
+    ])
+    setProp(person,'icon','http://placekeanu.com/64/64/d')
+    setProp(person,'addresses',[
+        {
+            type:CATEGORIES.CONTACT.TYPES.MAILING_ADDRESS,
+            props: {
+                type: 'home',
+                street1: '4055 Eddystone Place',
+                city: 'Eugene',
+                state: 'OR',
+                zipcode: '97404',
+                country: 'USA'
+            }
+        }
+    ])
+
+    // setProp(task,'title',"a cool task")
+    // setProp(task,'completed',true)
+    // setProp(task,'notes',"this is some very long notes to read. Like super duper long. So you're goint o have to spend some time reading this.")
+    return person
+}
+
+
 export const Default = () => {
-    let [obj] = useState(() => gen_data())
+    let [obj] = useState(() => make_task())
     return <StandardViewPanel object={obj}/>
 }
 
 export const HideProps = () => {
-    let [obj] = useState(() => gen_data())
+    let [obj] = useState(() => make_task())
     return <StandardViewPanel object={obj}
                               hide={["project",'deleted']}
     />
 }
 
 export const OrderProps = () => {
-    let [obj] = useState(() => gen_data())
+    let [obj] = useState(() => make_task())
     return <StandardViewPanel object={obj}
                               hide={['project','deleted']}
                               order={['title','notes']}
@@ -43,7 +96,7 @@ export const OrderProps = () => {
 }
 
 export const CustomizeProps = () => {
-    let [obj] = useState(() => gen_data())
+    let [obj] = useState(() => make_task())
     return <StandardViewPanel object={obj}
                               hide={["project"]}
                               custom={{
@@ -52,4 +105,10 @@ export const CustomizeProps = () => {
                                   'notes':'paragraph'
                               }}
     />
+}
+
+
+export const Contact = () => {
+    let [obj] = useState(() => make_contact())
+    return <StandardViewPanel object={obj}/>
 }
