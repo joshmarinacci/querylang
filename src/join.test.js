eac02e2ea4c4c7304a0a115323c318dc1518c7a2
josh <joshua@marinacci.org>
2020-12-07 22:28:08 -0800

    code cleanup and move engine to new query3.js file

--- src/join.test.js
+++ src/join.test.js
@@ -7,6 +7,7 @@ import '@testing-library/jest-dom/extend-expect';
 import {AND, IS_CATEGORY, IS_PROP_EQUAL, IS_PROP_TRUE, IS_TYPE, query2} from './query2.js'
 import {CATEGORIES} from './schema.js'
 import {makeDB, project} from './db.js'
+import {EXECUTE} from './query3.js'
 
 let db = makeDB()
 
@@ -18,7 +19,7 @@ it("get all favorite contacts",()=>{
         IS_TYPE(CATEGORIES.CONTACT.TYPES.PERSON),
         IS_PROP_TRUE("favorite")
     )
-    EXECUTE(favorites).then((res)=>{
+    EXECUTE(db,favorites).then((res)=>{
         console.log("result is",res)
         expect(res.length).toEqual(2)
         expect(res[0].props.first).toEqual('Jesse')
@@ -27,9 +28,9 @@ it("get all favorite contacts",()=>{
 })
 it("project addresses and first names",()=>{
     expect.assertions(1)
-    return EXECUTE(
-        PEXPAND(
-            PPROJECT(
+    return EXECUTE(db,
+        EXPAND(
+            PROJECT(
                 AND(
                     IS_DOMAIN('local'),
                     IS_CATEGORY(CATEGORIES.CONTACT.ID),
@@ -48,7 +49,7 @@ it("project addresses and first names",()=>{
 })
 it("fetch weather for city", () => {
     expect.assertions(1);
-    return EXECUTE(PJOIN(
+    return EXECUTE(db,JOIN(
         ONE(),
         JOIN_SOURCE(
             IS_DOMAIN("weather"),
@@ -67,80 +68,6 @@ function log(...args) {
     }).join("\n"))
 }
 
-function get_item_prop_by_name(item, name) {
-    // log("name is",name)
-    if(Array.isArray(name)) {
-        let path = name.slice()
-        let val = item
-        let lastname = name
-        while(path.length > 0) {
-            lastname = path.shift()
-            val = val.props[lastname]
-            // log('now val is',lastname,":",val)
-        }
-        return {
-            key:lastname,
-            value:val,
-        }
-    }
-    return {
-        key:name,
-        value:item.props[name]
-    }
-}
-
-function fetchWeather(item,conditions) {
-    log("fetching weather with conditions",conditions,'from item',item)
-    let q = conditions.map(cond => {
-        if(cond.on.value) return cond.on.value
-        return get_item_prop_by_name(item,cond.on.A).value
-    }).join(" ")
-    let api_key = 'fe5dd0b0a21045f0bdd235656201411'
-    let url = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${q}`
-    log("final url is",url)
-    return fetch(url)
-        .then(res => res.json())
-        .catch(e => {
-            log("error",e)
-            return []
-        })
-        .then(d => {
-            log("final result is",d)
-            return {
-                domain:'weatherapi',
-                category:'weatherapi',
-                type:'current',
-                props:{
-                    temp_c: d.current.temp_c,
-                    temp_f: d.current.temp_f,
-                }
-            }
-        })
-
-}
-
-function fetchCityInfo(item, mapping) {
-    log("fetching city info  with mapping",mapping,'from item',item)
-    let q = {}
-    mapping.forEach(cond => {
-        if(cond.on.value) {
-            q[cond.on.field] = cond.on.value
-        } else {
-            q[cond.on.B] = get_item_prop_by_name(item, cond.on.A).value
-        }
-    })
-    console.log('query is',q)
-    let result = {
-        type:'info',
-        props:{
-            timezone:'N/A'
-        }
-    }
-    if(q.city.toLowerCase() === 'eugene') {
-        result.props.timezone = 'Pacific'
-    }
-    return Promise.resolve(result)
-}
 function IS_DOMAIN(domain) {
     return {
         domain:domain,
@@ -171,13 +98,13 @@ it("join addresses to weather",()=>{
         IS_TYPE(CATEGORIES.CONTACT.TYPES.PERSON),
         IS_PROP_TRUE("favorite"),
     )
-    let clean_favs = PEXPAND(PPROJECT(favs,"first",'addresses'),'addresses')
-    let with_weather = PJOIN(clean_favs, JOIN_SOURCE(
+    let clean_favs = EXPAND(PROJECT(favs,"first",'addresses'),'addresses')
+    let with_weather = JOIN(clean_favs, JOIN_SOURCE(
         IS_DOMAIN("weather"),
         ON_EQUAL(["addresses","city"],"city"),
         ON_EQUAL(["addresses","state"],"state")
     ))
-    return EXECUTE(with_weather).then(data => {
+    return EXECUTE(db,with_weather).then(data => {
         log("final data",data)
         expect(data.length).toEqual(2)
         expect(data[0].props.first).toEqual("Jesse")
@@ -185,119 +112,10 @@ it("join addresses to weather",()=>{
 })
 
 
-function EXECUTE_AND(clause) {
-    log("EXECUTE_AND",clause)
-    let data = []
-    clause.forEach(c => {
-        if (c.domain && c.domain === 'local') {
-            data = db.data
-            return
-        }
-        if(c.CATEGORY) {
-            data = data.filter(it => it.category === c.CATEGORY)
-        }
-        if(c.TYPE) {
-            data = data.filter(it => it.type === c.TYPE)
-        }
-        if(c.equal) {
-            data = data.filter(it => it.props[c.equal.prop] === c.equal.value)
-        }
-    })
-    log("end of and is",data)
-    return Promise.resolve(data)
-}
-function EXECUTE_PROJECT(data, ...fields) {
-    log("projection is",fields,'on data',data)
-    return EXECUTE(data).then(data => {
-        log("real data is",data)
-        data = data.map(item => {
-            let obj = {props:{}}
-            fields.forEach(field => {
-                let dd = get_item_prop_by_name(item,field)
-                obj.props[dd.key] = dd.value
-            })
-            return obj
-        })
-        return data
-    })
-}
-function EXECUTE_EXPAND(data, field) {
-    log("EXECUTE_EXPAND",field,'on data',data)
-    return EXECUTE(data).then(data => {
-        let d2 = []
-        data.forEach(item => {
-            let vals = item.props[field]
-            vals.forEach(val => {
-                // console.log('value of',field,'is',val)
-                let obj = {props:{}}
-                Object.keys(item.props).forEach(key => {
-                    if(key !== field)  obj.props[key] = item.props[key]
-                })
-                obj.props[field] = val;
-                d2.push(obj)
-                // console.log("adding",obj)
-            })
-        })
-        return d2
-    })
-}
-function EXECUTE_NONE() {
-    log("EXECUTE_NONE")
-    return Promise.resolve([])
-}
-function EXECUTE_ONE() {
-    log("EXECUTE_ONE")
-    return Promise.resolve([{type:'one',props:{}}])
-}
-function EXECUTE_JOIN(data, join_source) {
-    log("EXECUTE_JOIN",join_source.join_source,'using data',data)
-    return EXECUTE(data).then(data => {
-        log("real data to join with is",data)
-        log("domain is", join_source.join_source.domain.domain)
-        log("mapping is", join_source.join_source.mapping)
-        let svc = null
-        if (join_source.join_source.domain.domain === 'weather') {
-            log("doing a weather query")
-            svc = fetchWeather
-        }
-        if(join_source.join_source.domain.domain === 'cityinfo') {
-            log("doing a city info query")
-            svc = fetchCityInfo
-        }
-        if(svc) {
-            return Promise.all(data.map(item => {
-                return svc(item,join_source.join_source.mapping).then(ret => {
-                    let item2 = {props:{}}
-                    Object.keys(item.props).forEach(key => {
-                        item2.props[key] = item.props[key]
-                    })
-                    Object.keys(ret.props).forEach(key => {
-                        item2.props[key] = ret.props[key]
-                    })
-                    return item2
-                })
-            }))
-        } else {
-            throw new Error("unsupported JOIN domain " + join_source.join_source.domain.domain)
-        }
-    })}
-function EXECUTE(...args) {
-    let proms = args.map(arg => {
-        log('EXECUTE',arg)
-        if(arg.project) return EXECUTE_PROJECT(...arg.project)
-        if(arg.expand) return EXECUTE_EXPAND(...arg.expand)
-        if(arg.join) return EXECUTE_JOIN(...arg.join)
-        if(arg.and) return EXECUTE_AND(arg.and)
-        if(arg.none) return EXECUTE_NONE()
-        if(arg.one) return EXECUTE_ONE()
-        return Promise.resolve("nothing")
-    })
-    return Promise.all(proms).then(d => d.flat())
-}
 
-const PPROJECT = (...args) => ({project:args})
-const PEXPAND = (...args) => ({expand:args})
-const PJOIN = (...args) => ({join:args})
+const PROJECT = (...args) => ({project:args})
+const EXPAND = (...args) => ({expand:args})
+const JOIN = (...args) => ({join:args})
 const NONE = () => ({none:'none'})
 const ONE  = () => ({one:'one'})
 
@@ -317,32 +135,32 @@ it("join addresses to city info server",()=>{
                 IS_TYPE(CATEGORIES.CONTACT.TYPES.PERSON),
                 IS_PROP_TRUE("favorite")
             )
-    // return EXECUTE(favorites).then(data => {
+    // return EXECUTE(db,favorites).then(data => {
     //     log("favorites",data)
     //     expect(data.length).toEqual(2)
     //     expect(data[0].props.first).toEqual("Jesse")
     // })
-    let just_names_and_addresses = PPROJECT(favorites,'first','addresses')
-    // return EXECUTE(just_names_and_addresses).then(data => {
+    let just_names_and_addresses = PROJECT(favorites,'first','addresses')
+    // return EXECUTE(db,just_names_and_addresses).then(data => {
     //     log("just names and addresses",data)
     //     expect(data.length).toEqual(2)
     //     expect(data[0].props.first).toEqual("Jesse")
     // })
-    let expanded_addresses = PEXPAND(just_names_and_addresses,"addresses")
-    // return EXECUTE(expanded_addresses).then(data => {
+    let expanded_addresses = EXPAND(just_names_and_addresses,"addresses")
+    // return EXECUTE(db,expanded_addresses).then(data => {
     //     log("expanded addresses is", data)
     //     expect(data.length).toEqual(2)
     //     expect(data[0].props.first).toEqual("Jesse")
     // })
 
     let none = NONE()
-    // return EXECUTE(none).then(data => {
+    // return EXECUTE(db,none).then(data => {
     //     log("none data is",data)
     //     expect(data.length).toEqual(0)
     //     expect(data.length).toEqual(0)
     // })
 
-    let static_weather = PJOIN(
+    let static_weather = JOIN(
         ONE(),
         JOIN_SOURCE(
             IS_DOMAIN("weather"),
@@ -352,13 +170,13 @@ it("join addresses to city info server",()=>{
             // ON_EQUAL(["addresses","state"],"state")
         )
     )
-    // return EXECUTE(static_weather).then(data => {
+    // return EXECUTE(db,static_weather).then(data => {
     //     log("static weather data is",data)
     //     expect(data.length).toEqual(1)
     //     expect(data[0].type).toEqual("current")
     // })
 
-    let dynamic_weather = PJOIN(
+    let dynamic_weather = JOIN(
         expanded_addresses,
         JOIN_SOURCE(
             IS_DOMAIN("weather"),
@@ -366,13 +184,13 @@ it("join addresses to city info server",()=>{
             ON_EQUAL(['addresses','state'],'state'),
         )
     )
-    // return EXECUTE(dynamic_weather).then(data => {
+    // return EXECUTE(db,dynamic_weather).then(data => {
     //     log("dynamic weather data is",data)
     //     expect(data.length).toEqual(2)
     //     expect(data[0].type).toEqual("current")
     // })
 
-    let dynamic_timezones = PJOIN(
+    let dynamic_timezones = JOIN(
         expanded_addresses,
         JOIN_SOURCE(
             IS_DOMAIN("cityinfo"),
@@ -380,7 +198,7 @@ it("join addresses to city info server",()=>{
             ON_EQUAL(['addresses','state'],'state'),
         )
     )
-    return EXECUTE(dynamic_timezones).then(data => {
+    return EXECUTE(db,dynamic_timezones).then(data => {
         log("dynamic timezone data is",data)
         expect(data.length).toEqual(2)
         // expect(data[0].type).toEqual("info")
---
+++ src/query3.js
@@ -0,0 +1,191 @@
+function log(...args) {
+    console.log(args.map(a => {
+        return JSON.stringify(a,null,'  ')
+    }).join("\n"))
+}
+function EXECUTE_AND(db,clause) {
+    log("EXECUTE_AND",clause)
+    let data = []
+    clause.forEach(c => {
+        if (c.domain && c.domain === 'local') {
+            data = db.data
+            return
+        }
+        if(c.CATEGORY) {
+            data = data.filter(it => it.category === c.CATEGORY)
+        }
+        if(c.TYPE) {
+            data = data.filter(it => it.type === c.TYPE)
+        }
+        if(c.equal) {
+            data = data.filter(it => it.props[c.equal.prop] === c.equal.value)
+        }
+    })
+    log("end of and is",data)
+    return Promise.resolve(data)
+}
+function EXECUTE_PROJECT(db,data, ...fields) {
+    log("projection is",fields,'on data',data)
+    return EXECUTE(db,data).then(data => {
+        log("real data is",data)
+        data = data.map(item => {
+            let obj = {props:{}}
+            fields.forEach(field => {
+                let dd = get_item_prop_by_name(item,field)
+                obj.props[dd.key] = dd.value
+            })
+            return obj
+        })
+        return data
+    })
+}
+function EXECUTE_EXPAND(db,data, field) {
+    log("EXECUTE_EXPAND",field,'on data',data)
+    return EXECUTE(db,data).then(data => {
+        let d2 = []
+        data.forEach(item => {
+            let vals = item.props[field]
+            vals.forEach(val => {
+                // console.log('value of',field,'is',val)
+                let obj = {props:{}}
+                Object.keys(item.props).forEach(key => {
+                    if(key !== field)  obj.props[key] = item.props[key]
+                })
+                obj.props[field] = val;
+                d2.push(obj)
+                // console.log("adding",obj)
+            })
+        })
+        return d2
+    })
+}
+function EXECUTE_NONE() {
+    log("EXECUTE_NONE")
+    return Promise.resolve([])
+}
+function EXECUTE_ONE() {
+    log("EXECUTE_ONE")
+    return Promise.resolve([{type:'one',props:{}}])
+}
+function EXECUTE_JOIN(db,data, join_source) {
+    log("EXECUTE_JOIN",join_source.join_source,'using data',data)
+    return EXECUTE(db,data).then(data => {
+        log("real data to join with is",data)
+        log("domain is", join_source.join_source.domain.domain)
+        log("mapping is", join_source.join_source.mapping)
+        let svc = null
+        if (join_source.join_source.domain.domain === 'weather') {
+            log("doing a weather query")
+            svc = fetchWeather
+        }
+        if(join_source.join_source.domain.domain === 'cityinfo') {
+            log("doing a city info query")
+            svc = fetchCityInfo
+        }
+        if(svc) {
+            return Promise.all(data.map(item => {
+                return svc(item,join_source.join_source.mapping).then(ret => {
+                    let item2 = {props:{}}
+                    Object.keys(item.props).forEach(key => {
+                        item2.props[key] = item.props[key]
+                    })
+                    Object.keys(ret.props).forEach(key => {
+                        item2.props[key] = ret.props[key]
+                    })
+                    return item2
+                })
+            }))
+        } else {
+            throw new Error("unsupported JOIN domain " + join_source.join_source.domain.domain)
+        }
+    })}
+export function EXECUTE(db,...args) {
+    let proms = args.map(arg => {
+        log('EXECUTE',arg)
+        if(arg.project) return EXECUTE_PROJECT(db,...arg.project)
+        if(arg.expand) return EXECUTE_EXPAND(db,...arg.expand)
+        if(arg.join) return EXECUTE_JOIN(db,...arg.join)
+        if(arg.and) return EXECUTE_AND(db,arg.and)
+        if(arg.none) return EXECUTE_NONE(db)
+        if(arg.one) return EXECUTE_ONE(db)
+        return Promise.resolve("nothing")
+    })
+    return Promise.all(proms).then(d => d.flat())
+}
+
+
+
+function get_item_prop_by_name(item, name) {
+    // log("name is",name)
+    if(Array.isArray(name)) {
+        let path = name.slice()
+        let val = item
+        let lastname = name
+        while(path.length > 0) {
+            lastname = path.shift()
+            val = val.props[lastname]
+            // log('now val is',lastname,":",val)
+        }
+        return {
+            key:lastname,
+            value:val,
+        }
+    }
+    return {
+        key:name,
+        value:item.props[name]
+    }
+}
+
+function fetchWeather(item,conditions) {
+    log("fetching weather with conditions",conditions,'from item',item)
+    let q = conditions.map(cond => {
+        if(cond.on.value) return cond.on.value
+        return get_item_prop_by_name(item,cond.on.A).value
+    }).join(" ")
+    let api_key = 'fe5dd0b0a21045f0bdd235656201411'
+    let url = `http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${q}`
+    log("final url is",url)
+    return fetch(url)
+        .then(res => res.json())
+        .catch(e => {
+            log("error",e)
+            return []
+        })
+        .then(d => {
+            log("final result is",d)
+            return {
+                domain:'weatherapi',
+                category:'weatherapi',
+                type:'current',
+                props:{
+                    temp_c: d.current.temp_c,
+                    temp_f: d.current.temp_f,
+                }
+            }
+        })
+
+}
+
+function fetchCityInfo(item, mapping) {
+    log("fetching city info  with mapping",mapping,'from item',item)
+    let q = {}
+    mapping.forEach(cond => {
+        if(cond.on.value) {
+            q[cond.on.field] = cond.on.value
+        } else {
+            q[cond.on.B] = get_item_prop_by_name(item, cond.on.A).value
+        }
+    })
+    console.log('query is',q)
+    let result = {
+        type:'info',
+        props:{
+            timezone:'N/A'
+        }
+    }
+    if(q.city.toLowerCase() === 'eugene') {
+        result.props.timezone = 'Pacific'
+    }
+    return Promise.resolve(result)
+}
