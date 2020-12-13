8999e2997c97ce4f7948a1955937943d0602c22c
josh <joshua@marinacci.org>
2020-12-13 12:09:00 -0800

    add bad test

--- scratch/tallycat/simple.js
+++ scratch/tallycat/simple.js
@@ -2,3 +2,10 @@ import {Parser} from "tallycat-parser";
 
 let result = Parser.parseString('4ft * 6');
 console.log(result.toString())
+
+
+try {
+    let bad_result = Parser.parseString(('4ft * '))
+} catch (e) {
+    console.error(e)
+}
